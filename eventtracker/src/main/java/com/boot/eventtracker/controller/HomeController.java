6eaaca414f265db12d6d83f992e72093b68f708a
Sam Adreddy <sampath.adreddy@gmail.com>
2019-06-17 23:07:09 -0400

    Initial commit.

---
+++ eventtracker/src/main/java/com/boot/eventtracker/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.boot.eventtracker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+	 return "Spring Boot!!!.";
+	}
+
+}
